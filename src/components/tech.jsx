2228c1f357830755c4aafa16118491a16b91c514
Shiva Gupta <shivagupta4639@gmail.com>
2021-11-13 15:20:25 +0530

    Tech Fix

--- src/components/tech.jsx
+++ src/components/tech.jsx
@@ -1,15 +1,23 @@
 export const Tech = (props) => {
-    return (
-      <div id="tech">
-        <div className="container">
-        <div className='section-title'>
-          <h2 style={{textAlign:"center"}}>Tech</h2>
+  return (
+    <div id="tech">
+      <div className="container">
+        <div className="section-title">
+          <h2 style={{ textAlign: "center" }}>Tech</h2>
         </div>
         <div>
-            <img src="img/Repurpose_v1_tech.jpeg" width="860" height="500" style=></img>
-        </div>
+          <img
+            src="img/Repurpose_v1_tech.jpeg"
+            width="860"
+            height="500"
+            style={{
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          ></img>
         </div>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
