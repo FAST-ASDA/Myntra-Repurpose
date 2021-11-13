911f1fab11fb2578eb8a96856c136651ead99e45
anikash-oxytocin <88781761+anikash-oxytocin@users.noreply.github.com>
2021-11-13 15:04:39 +0530

    style: :fire: Added tech and swot

---
+++ src/components/competitors.jsx
@@ -0,0 +1,87 @@
+export const Competitors = (props) => {
+
+  return (
+    <div>
+      <div id='competitors'>
+        <div className='container'>
+        <div className='section-title'>
+          <h2>SWOT Analysis</h2>
+        </div>
+          <div class="main-container">
+            <div class="cards">
+              <div class="card card-4">
+                <div class="card__icon"><i class="fas fa-bolt"></i> <span class="mx-3">Strength</span></div>
+                <p class="card__exit"><i class="fas fa-times"></i></p>
+                <ol>
+                  <li>
+                    <h4 class="card__title">Influencer brand promotion through the social platform. </h4>
+                  </li>
+                  <li>
+                    <h4 class="card__title">First mover advantage. </h4>
+                  </li>
+                  <li>
+                    <h4 class="card__title">Easier luxury brand discovery due to reduced prices. </h4>
+                  </li>
+                    <li>
+                      <h4 class="card__title">Environment friendly.</h4>
+                    </li>
+                  
+                </ol>
+                
+              </div>
+              <div class="card card-1">
+                <div class="card__icon"><i class="fas fa-bolt"></i> <span class="mx-3">Weakness</span></div>
+                <p class="card__exit"><i class="fas fa-times"></i> </p>
+                <ol>
+                  <li>
+                    <h4 class="card__title">Developing trust in users comparatively difficult. </h4>
+                  </li>
+                  <li>
+                    <h4 class="card__title">Manual quality assurance required </h4>
+                  </li>
+                </ol>
+              </div>
+              <div class="card card-3">
+                <div class="card__icon"><i class="fas fa-bolt"></i> <span class="mx-3">Opportunity</span></div>
+                <p class="card__exit"><i class="fas fa-times"></i></p>
+                  <ol>
+                    <li>
+                      <h4 class="card__title">Capturing deep seated markets in India</h4>
+                    </li>
+                    <li>
+                      <h4 class="card__title">Increase in user base</h4>
+                    </li>
+                    <li>
+                      <h4 class="card__title">Major Marketplace for Thrifting</h4>
+                    </li>
+                    <li>
+                      <h4 class="card__title">International Market Expansion</h4>
+                    </li>
+                  </ol>
+          
+              </div>
+              <div class="card card-2">
+                <div class="card__icon"><i class="fas fa-bolt"></i> <span class="mx-3">Threat</span></div>
+                <p class="card__exit"><i class="fas fa-times"></i></p>
+                <ol>
+                  <li>
+                    <h4 class="card__title">Unorganised sector</h4>
+                  </li>
+                  <li>
+                    <h4 class="card__title">Competition from social platform rivals</h4>
+                  </li>
+                  <li>
+                    <h4 class="card__title">Changing consumer preferrences</h4>
+                  </li>
+                </ol>
+              </div>
+              
+              
+            </div>
+          </div>
+         </div>
+      </div>
+      
+    </div>
+  )
+}
---
+++ src/components/tech.jsx
@@ -0,0 +1,15 @@
+export const Tech = (props) => {
+    return (
+      <div id="tech">
+        <div className="container">
+        <div className='section-title'>
+          <h2 style={{textAlign:"center"}}>Tech</h2>
+        </div>
+        <div>
+            <img src="img/Repurpose_v1_tech.jpeg" width="860" height="500" style=></img>
+        </div>
+        </div>
+      </div>
+    );
+  };
+  
